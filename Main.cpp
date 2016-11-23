d300926c42babc8eb0ce4e640b653ec80579d087
MyNameIsLewis <lewiskearsey@gmail.com>
2016-11-23 18:06:59 +0000

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -15,12 +15,23 @@ public:
 	double getISBN() const { return ISBN; }
 	Book* getNext() const { return next; }
 	void setNext(Book* nextParam) { next = nextParam; }
+	Book* getPrevious() const { return previous; }
+	void setPrevious(Book* previousParam)
+	{
+		previous = previousParam;
+	}
+	void setCurrent(Book* currentParam)
+	{
+		current = currentParam;
+	}
 
 private:
 	string name;
 	string author;
 	double ISBN;
 	Book* next;
+	Book* previous; //Previous book on the list
+	Book* current; //Current book on the list
 
 };
 
@@ -42,7 +53,43 @@ void printBookDetails(Book *first)
 		nextBook = nextBook->getNext();
 	}
 }
+void removeBook(Book* toRemove, Book* previousBook, Book* nextBook)
+{
+	assert(toRemove != NULL);
+	previousBook->setNext(toRemove -> getNext());
+	nextBook->setPrevious(toRemove -> getPrevious());
+	toRemove->setNext(NULL);
+	toRemove->setPrevious(NULL);
+	toRemove->setCurrent(NULL);
+}
+void addInFront(Book* toAdd, Book* inFrontOf)
+{
+	assert(inFrontOf != NULL);
+	toAdd->setNext(inFrontOf -> getNext());
+	inFrontOf->setNext(toAdd);
+}
 
+void addBefore(Book* toAdd, Book* before, Book* inFrontOf)
+{
+	assert(inFrontOf != NULL);
+	toAdd->setNext(before);
+	before->setPrevious(toAdd);
+	inFrontOf->setNext(toAdd);
+	toAdd -> setPrevious(inFrontOf);
+}
+
+void printPreviousDetails (Book *last)
+	{
+		Book* previousBook = last;
+		while(previousBook != NULL)
+		{
+			//Print details
+			cout << "ISBN: "<<previousBook-> getISBN() << endl;
+
+			//Get privious customer
+			previousBook = previousBook -> getPrevious();
+		}
+	}
 Book* searchForBookByISBN(double isbnNum, Book* first)
 {
 	Book* nextBook = first;
@@ -143,9 +190,15 @@ int main()
 	string nameInput;
 	double ISBNInput;
 	int chooseInput;
+	int password;
 	//printBookDetails(potter);
 
 	while (1 < 2)
+	{
+	cout << "Hello! Please tell me what to do!" << endl << "Press 1 for Book Search" << endl << "Press 2 for book addition (Requires admin login)" << "Press 3 for Book Deletion (Requires admin login)" << endl << "Press 4 for Book rental/return";
+	cin >> chooseInput;
+	
+	if (chooseInput == 1)
 	{
 	cout << "Please select how you wish to search by: " << endl << "Press 1 for book name" << endl << "Press 2 for Author name" << endl << "Press 3 for ISBN" << endl << "Press 4 to quit"  << endl;
 	cin >> chooseInput;
@@ -191,9 +244,21 @@ int main()
 			cout << "Book not found." << endl << endl;
 		}
 	}
+	
 	if (chooseInput == 4)
 	{
 		return 0;
+	}
+	}
+
+	if (chooseInput == 2)
+	{
+		cout << "Please enter password" << endl;
+		cin >> password;
+		if (password == 12345)
+		{
+			cout << "Welcome back!" << endl << "Please enter the ID of the book you wish to add, the book before it and the book after it" << endl;
+		}
 	}
 	}
 }
